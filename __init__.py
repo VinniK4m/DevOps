688eef5202939116f3e080629a041d75e83e73d1
Albeiro Cuadrado <a.cuadrado@uniandes.edu.co>
2022-04-05 00:14:36 -0500

    Env var setup

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
+import os
+
 from flask import Flask
+
+hostname = os.environ['RDS_HOST']
+user = os.environ['RDS_USERNAME']
+password = os.environ['RDS_PASSWORD']
+dbname = os.environ['RDS_DATABASE']
 
 
 def create_app(config_name):
     app = Flask(__name__)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://elusuario:12345@localhost:5432/blacklist'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blacklist.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{hostname}/{dbname}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'no-hay-frase'
-    app.config['PROPAGATE_EXCEPTIONS'] = True
     return app
