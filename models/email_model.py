d4cd898bcb445b3bf3641bec0ec47ae1e5bba753
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2022-04-05 00:22:09 -0500

    Ajusta nombres de campos según documentación

--- models/email_model.py
+++ models/email_model.py
@@ -10,9 +10,9 @@ class Email(db.Model):
     __tablename__ = "email"
 
     email = Column(db.String(128),primary_key=True, unique=True)
-    UUID = Column(db.Integer)
-    reason = Column(db.String(128))
-    ip = Column(db.String(50))
+    app_uuid = Column(db.Integer)
+    blocked_reason = Column(db.String(255))
+    ip_client = Column(db.String(50))
     date_create = Column(DateTime(timezone=True), server_default=func.now())
 
 
